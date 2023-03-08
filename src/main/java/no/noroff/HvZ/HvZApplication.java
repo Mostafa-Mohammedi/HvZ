cc453319ead41ed3aff81e9fe3097defb3e52c59
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-03-08 08:51:03 +0100

    Initial commit

---
+++ src/main/java/no/noroff/HvZ/HvZApplication.java
@@ -0,0 +1,13 @@
+package no.noroff.HvZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/noroff/HvZ/HvZApplicationTests.java
@@ -0,0 +1,13 @@
+package no.noroff.HvZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
