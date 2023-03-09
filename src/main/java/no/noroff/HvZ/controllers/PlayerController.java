7744987c405ee7429e80acb00060eaeb562a6e8f
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-03-09 09:12:28 +0100

    added cors configuration for controller

--- src/main/java/no/noroff/HvZ/controllers/PlayerController.java
+++ src/main/java/no/noroff/HvZ/controllers/PlayerController.java
@@ -15,8 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/v1/players")
 public class PlayerController {
--- src/main/java/no/noroff/HvZ/controllers/SquadController.java
+++ src/main/java/no/noroff/HvZ/controllers/SquadController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/v1/squads")
 public class SquadController {
